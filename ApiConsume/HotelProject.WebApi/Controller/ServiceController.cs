a5c05d02f511f5475a7a02fd0bd8bd2037f145e1
BULUTS <BULUTS@BULUTS>
2023-05-07 09:53:43 +0300

    42. Login İşlemi

--- ApiConsume/HotelProject.WebApi/Controller/ServiceController.cs
+++ ApiConsume/HotelProject.WebApi/Controller/ServiceController.cs
@@ -28,7 +28,7 @@ namespace HotelProject.WebApi.Controller
             _serviceService.TInsert(service);
             return Ok();
         }
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public IActionResult DeleteService(int id)
         {
             var values = _serviceService.TGetByID(id);
