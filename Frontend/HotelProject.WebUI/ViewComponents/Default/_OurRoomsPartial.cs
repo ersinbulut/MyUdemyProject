72850192ba2d03a71c168a0c7665966451dddc38
BULUTS <BULUTS@BULUTS>
2023-05-14 14:10:49 +0300

    49. Trailer Düzenlemesi

--- Frontend/HotelProject.WebUI/ViewComponents/Default/_OurRoomsPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_OurRoomsPartial.cs
@@ -1,11 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.RoomDto;
+using HotelProject.WebUI.Dtos.ServiceDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _OurRoomsPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _OurRoomsPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var resonseMessage = await client.GetAsync("http://localhost:61639/api/Room");
+            if (resonseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await resonseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultRoomDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
