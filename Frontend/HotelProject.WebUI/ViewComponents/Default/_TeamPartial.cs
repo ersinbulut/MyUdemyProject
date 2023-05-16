9fc3d208b6704e2b908de8f7c133d04a0d6fe765
BULUTS <BULUTS@BULUTS>
2023-05-16 16:22:16 +0300

    55. Footer Düzenlemesi

--- Frontend/HotelProject.WebUI/ViewComponents/Default/_TeamPartial.cs
+++ Frontend/HotelProject.WebUI/ViewComponents/Default/_TeamPartial.cs
@@ -1,11 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelProject.WebUI.Dtos.ServiceDto;
+using HotelProject.WebUI.Dtos.StaffDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace HotelProject.WebUI.ViewComponents.Default
 {
     public class _TeamPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TeamPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var resonseMessage = await client.GetAsync("http://localhost:61639/api/Staff");
+            if (resonseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await resonseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultStaffDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
