2d6ff5db1c1556a635e5662944cd2906cc6d6668
BULUTS <BULUTS@BULUTS>
2023-05-18 11:24:05 +0300

    59. Booking İşlemi

--- ApiConsume/HotelProject.WebApi/Startup.cs
+++ ApiConsume/HotelProject.WebApi/Startup.cs
@@ -49,7 +49,11 @@ namespace HotelProject.WebApi
 			services.AddScoped<IAboutDal, EfAboutDal>();
 			services.AddScoped<IAboutService, AboutManager>();
 
-			services.AddAutoMapper(typeof(Startup));
+
+            services.AddScoped<IBookingDal, EfBookingDal>();
+            services.AddScoped<IBookingService, BookingManager>();
+
+            services.AddAutoMapper(typeof(Startup));
 
             //services.AddScoped
 
