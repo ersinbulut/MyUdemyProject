a2357e87c34e21428fdc2d03a5773cf81885a1a5
BULUTS <BULUTS@BULUTS>
2023-05-18 11:44:29 +0300

    60. Booking Admin Sayfası

---
+++ Frontend/HotelProject.WebUI/Controllers/BookingAdminController.cs
@@ -0,0 +1,33 @@
+﻿using HotelProject.WebUI.Dtos.BookingDto;
+using HotelProject.WebUI.Dtos.ServiceDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class BookingAdminController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public BookingAdminController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var resonseMessage = await client.GetAsync("http://localhost:61639/api/Booking");
+            if (resonseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await resonseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBookingDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
