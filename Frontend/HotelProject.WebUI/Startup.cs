b536e9f4d20e318fd74ebd64b0e81faf6a371a72
BULUTS <BULUTS@BULUTS>
2023-07-27 11:36:39 +0300

    125. 404 Error Sayfası

--- Frontend/HotelProject.WebUI/Startup.cs
+++ Frontend/HotelProject.WebUI/Startup.cs
@@ -4,8 +4,10 @@ using HotelProject.DataAccessLayer.Concrete;
 using HotelProject.EntityLayer.Concrete;
 using HotelProject.WebUI.Dtos.GuestDto;
 using HotelProject.WebUI.ValidationRules.GuestValidationRules;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -34,6 +36,19 @@ namespace HotelProject.WebUI
             services.AddTransient<IValidator<CreateGuestDto>,CreateGuestValidator>();
             services.AddTransient<IValidator<UpdateGuestDto>,UpdateGuestValidator>();
             services.AddControllersWithViews().AddFluentValidation();
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            });
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(10);
+                options.LoginPath = "/Login/Index/";
+            });
             services.AddAutoMapper(typeof(Startup));
         }
 
@@ -48,7 +63,13 @@ namespace HotelProject.WebUI
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            app.UseStatusCodePagesWithReExecute("/ErrorPage/Error404","?code={0}");
+            app.UseHttpsRedirection();
+
             app.UseStaticFiles();
+
+            app.UseAuthentication();
 
             app.UseRouting();
 
