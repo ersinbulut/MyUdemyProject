29f84edddef1a012a8f433319963aec6ef771520
BULUTS <BULUTS@BULUTS>
2023-07-28 07:58:15 +0300

    133. Dropdown Aracı ile İlişkili Başlıkların Çekilmesi

--- Frontend/HotelProject.WebUI/Controllers/ContactController.cs
+++ Frontend/HotelProject.WebUI/Controllers/ContactController.cs
@@ -1,9 +1,14 @@
 ﻿using HotelProject.WebUI.Dtos.BookingDto;
 using HotelProject.WebUI.Dtos.ContactDto;
+using HotelProject.WebUI.Dtos.MessageCategoryDto;
+using HotelProject.WebUI.Dtos.RoomDto;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +24,23 @@ namespace HotelProject.WebUI.Controllers
         {
             _httpClientFactory = httpClientFactory;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+			var client = _httpClientFactory.CreateClient();
+			var resonseMessage = await client.GetAsync("http://localhost:61639/api/MessageCategory");
+
+			var jsonData = await resonseMessage.Content.ReadAsStringAsync();
+			var values = JsonConvert.DeserializeObject<List<ResultMessageCategoryDto>>(jsonData);
+
+            List<SelectListItem> values2 = (from x in values
+                                            select new SelectListItem
+                                            {
+                                                Text=x.MessageCategoryName,
+                                                Value=x.MessageCategoryID.ToString()
+                                            }).ToList();
+            ViewBag.values2 = values2;
+
+			return View();
         }
 
         [HttpGet]
