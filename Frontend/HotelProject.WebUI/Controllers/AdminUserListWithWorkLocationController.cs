02971185466761e97941878ea43c2b80a99bba24
Dev <Dev@DESKTOP-L10DGNS>
2023-08-18 17:20:44 +0300

    151. Sorunu Anlamak

---
+++ Frontend/HotelProject.WebUI/Controllers/AdminUserListWithWorkLocationController.cs
@@ -0,0 +1,32 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.WebUI.Dtos.AppUserDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HotelProject.WebUI.Controllers
+{
+    public class AdminUserListWithWorkLocationController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AdminUserListWithWorkLocationController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> UserList()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var resonseMessage = await client.GetAsync("http://localhost:61639/api/AppUser");
+            if (resonseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await resonseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAppUserWithWorkLocationDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
